cb22661190dd97fe0b2969f3fd77aefcbd9c4392
phprao <root@localhost.localdomain>
2020-08-03 09:04:40 +0800

    modify

--- src/interfaces/CalculatorServiceInterface.php
+++ src/interfaces/CalculatorServiceInterface.php
@@ -6,4 +6,6 @@ namespace phprao\composer-test\interfaces;
 interface CalculatorServiceInterface
 {
     public function add(int $a, int $b): int;
+
+    public function minus(int $a, int $b): int;
 }
