cd232157a85dab6e27356ff41b97b92e00042eac
phprao <728067836@qq.com>
2020-08-03 10:01:50 +0800

    modify

--- src/interfaces/CalculatorServiceInterface.php
+++ src/interfaces/CalculatorServiceInterface.php
@@ -6,6 +6,4 @@ namespace Phprao\ComposerTest\Interfaces;
 interface CalculatorServiceInterface
 {
     public function add(int $a, int $b): int;
-
-    public function minus(int $a, int $b): int;
 }
